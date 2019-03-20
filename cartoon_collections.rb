1252d1351e5a09962c961d2a8b1e92732d877f1a
David Cantoran <davidcantoran36@gmail.com>
2019-03-20 23:51:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(queso)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find? do |name|
+    
+    
 end
