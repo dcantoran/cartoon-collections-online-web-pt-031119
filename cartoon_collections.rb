1ad3d2c55f6d1d45a97b096db161be01aa3210a2
David Cantoran <davidcantoran36@gmail.com>
2019-03-21 00:00:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(queso)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   queso.find do |name|
-    name.include?
+    name.include?("#{cheese_types}")
   end
     
 end
